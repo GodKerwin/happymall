5cc22da7b1ef42b5a97b876d577feeef7b3c0f66
xul <513624876@qq.com>
2019-05-20 17:51:42 +0800

    修改Druid配置

--- happymall-base/src/main/java/com/xul/happymall/base/support/config/DruidConfiguration.java
+++ happymall-base/src/main/java/com/xul/happymall/base/support/config/DruidConfiguration.java
@@ -1,256 +1,50 @@
 package com.xul.happymall.base.support.config;
 
-import com.alibaba.druid.filter.config.ConfigTools;
 import com.alibaba.druid.pool.DruidDataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
-import java.sql.SQLException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by lxu on 2018/12/28.
  */
 @Configuration
-@ConfigurationProperties(prefix = "spring.datasource")
 public class DruidConfiguration {
-    private static Logger logger = LoggerFactory.getLogger(DruidConfiguration.class);
 
-    private String url;
-
-    private String username;
-
-    private String password;
-
-    private String publicKey;
-
-    private String driverClassName;
-
-    private int initialSize;
-
-    private int minIdle;
-
-    private int maxActive;
-
-    private int maxWait;
-
-    private int timeBetweenEvictionRunsMillis;
-
-    private int minEvictableIdleTimeMillis;
-
-    private String validationQuery;
-
-    private boolean testWhileIdle;
-
-    private boolean testOnBorrow;
-
-    private boolean testOnReturn;
-
-    private boolean poolPreparedStatements;
-
-    private int maxPoolPreparedStatementPerConnectionSize;
-
-    private String filters;
-
-    private String connectionProperties;
-
-    private boolean useGlobalDataSourceStat;
-
-    @Bean // 声明其为Bean实例
-    @Primary // 在同样的DataSource中，首先使用被标注的DataSource
-    public DataSource dataSource() throws Exception {
-
-        DruidDataSource datasource = new DruidDataSource();
-
-        datasource.setUrl(url);
-        datasource.setUsername(username);
-        datasource.setPassword(ConfigTools.decrypt(publicKey, password));
-        datasource.setDriverClassName(driverClassName);
-
-        // configuration
-        datasource.setInitialSize(initialSize);
-        datasource.setMinIdle(minIdle);
-        datasource.setMaxActive(maxActive);
-        datasource.setMaxWait(maxWait);
-        datasource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
-        datasource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
-        datasource.setValidationQuery(validationQuery);
-        datasource.setTestWhileIdle(testWhileIdle);
-        datasource.setTestOnBorrow(testOnBorrow);
-        datasource.setTestOnReturn(testOnReturn);
-        datasource.setPoolPreparedStatements(poolPreparedStatements);
-        datasource.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);
-        try {
-            datasource.setFilters(filters);
-        } catch (SQLException e) {
-            logger.error("druid configuration initialization filter", e);
-        }
-        datasource.setConnectionProperties(connectionProperties);
-        datasource.setUseGlobalDataSourceStat(useGlobalDataSourceStat);
-        return datasource;
+    // 将所有前缀为spring.datasource下的配置项都加载到DataSource中
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @Bean
+    public DataSource druidDataSource() {
+        return new DruidDataSource();
     }
 
-    public String getUrl() {
-        return url;
+    @Bean
+    public ServletRegistrationBean druidStatViewServlet() {
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(),"/druid/*");
+        Map<String, String> initParams = new HashMap<>();
+        //　可配的属性都在 StatViewServlet 和其父类下
+        initParams.put("loginUsername", "admin");
+        initParams.put("loginPassword", "admin");
+        servletRegistrationBean.setInitParameters(initParams);
+        return servletRegistrationBean;
     }
 
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPublicKey() {
-        return publicKey;
-    }
-
-    public void setPublicKey(String publicKey) {
-        this.publicKey = publicKey;
-    }
-
-    public String getDriverClassName() {
-        return driverClassName;
-    }
-
-    public void setDriverClassName(String driverClassName) {
-        this.driverClassName = driverClassName;
-    }
-
-    public int getInitialSize() {
-        return initialSize;
-    }
-
-    public void setInitialSize(int initialSize) {
-        this.initialSize = initialSize;
-    }
-
-    public int getMinIdle() {
-        return minIdle;
-    }
-
-    public void setMinIdle(int minIdle) {
-        this.minIdle = minIdle;
-    }
-
-    public int getMaxActive() {
-        return maxActive;
-    }
-
-    public void setMaxActive(int maxActive) {
-        this.maxActive = maxActive;
-    }
-
-    public int getMaxWait() {
-        return maxWait;
-    }
-
-    public void setMaxWait(int maxWait) {
-        this.maxWait = maxWait;
-    }
-
-    public int getTimeBetweenEvictionRunsMillis() {
-        return timeBetweenEvictionRunsMillis;
-    }
-
-    public void setTimeBetweenEvictionRunsMillis(int timeBetweenEvictionRunsMillis) {
-        this.timeBetweenEvictionRunsMillis = timeBetweenEvictionRunsMillis;
-    }
-
-    public int getMinEvictableIdleTimeMillis() {
-        return minEvictableIdleTimeMillis;
-    }
-
-    public void setMinEvictableIdleTimeMillis(int minEvictableIdleTimeMillis) {
-        this.minEvictableIdleTimeMillis = minEvictableIdleTimeMillis;
-    }
-
-    public String getValidationQuery() {
-        return validationQuery;
-    }
-
-    public void setValidationQuery(String validationQuery) {
-        this.validationQuery = validationQuery;
-    }
-
-    public boolean isTestWhileIdle() {
-        return testWhileIdle;
-    }
-
-    public void setTestWhileIdle(boolean testWhileIdle) {
-        this.testWhileIdle = testWhileIdle;
-    }
-
-    public boolean isTestOnBorrow() {
-        return testOnBorrow;
-    }
-
-    public void setTestOnBorrow(boolean testOnBorrow) {
-        this.testOnBorrow = testOnBorrow;
-    }
-
-    public boolean isTestOnReturn() {
-        return testOnReturn;
-    }
-
-    public void setTestOnReturn(boolean testOnReturn) {
-        this.testOnReturn = testOnReturn;
-    }
-
-    public boolean isPoolPreparedStatements() {
-        return poolPreparedStatements;
-    }
-
-    public void setPoolPreparedStatements(boolean poolPreparedStatements) {
-        this.poolPreparedStatements = poolPreparedStatements;
-    }
-
-    public int getMaxPoolPreparedStatementPerConnectionSize() {
-        return maxPoolPreparedStatementPerConnectionSize;
-    }
-
-    public void setMaxPoolPreparedStatementPerConnectionSize(int maxPoolPreparedStatementPerConnectionSize) {
-        this.maxPoolPreparedStatementPerConnectionSize = maxPoolPreparedStatementPerConnectionSize;
-    }
-
-    public String getFilters() {
-        return filters;
-    }
-
-    public void setFilters(String filters) {
-        this.filters = filters;
-    }
-
-    public String getConnectionProperties() {
-        return connectionProperties;
-    }
-
-    public void setConnectionProperties(String connectionProperties) {
-        this.connectionProperties = connectionProperties;
-    }
-
-    public boolean isUseGlobalDataSourceStat() {
-        return useGlobalDataSourceStat;
-    }
-
-    public void setUseGlobalDataSourceStat(boolean useGlobalDataSourceStat) {
-        this.useGlobalDataSourceStat = useGlobalDataSourceStat;
+    @Bean
+    public FilterRegistrationBean druidWebStatFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
+        Map<String, String> initParams = new HashMap<>();
+        initParams.put("exclusions", "*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*");
+        filterRegistrationBean.setInitParameters(initParams);
+        filterRegistrationBean.setUrlPatterns(Collections.singletonList("/*"));
+        return filterRegistrationBean;
     }
 }
